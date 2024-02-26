5bf06b967a1a40c0d22e6a1bc1e6ad82821fe079
BrunoRaul1 <brunosouza.0328@gmail.com>
2024-02-26 18:45:30 -0300

    ícone de campeões

--- script.js
+++ script.js
@@ -9,3 +9,33 @@ openModalButton.addEventListener('click', () => {
 closeModalButton.addEventListener('click', () => {
   modal.close()
 })
+
+const input = document.querySelector(".guess-input")
+const list = document.querySelector(".champion-list")
+
+fetch('https://ddragon.leagueoflegends.com/cdn/14.4.1/data/pt_BR/champion.json')
+  .then(response => response.json())
+  .then(object => {
+    let champions = Object.keys(object.data)
+
+    input.addEventListener('input', (event) => {
+      list.innerHTML = ""
+
+      if (event.target.value == "") return
+
+      const filteredChampions = champions.filter(champion => champion.toLowerCase().includes(event.target.value))
+
+      filteredChampions.forEach((champion) => {
+
+        const imgUrl = `https://ddragon.leagueoflegends.com/cdn/14.4.1/img/champion/${champion}.png`
+
+        list.innerHTML += `
+          <li class="option">
+            <input type="radio" name="teste" id="teste">
+            <img src="${imgUrl}" width="48px" height="48px"/>
+            <p>${champion}</p>
+          </li>
+        `
+      })
+    })
+  })
