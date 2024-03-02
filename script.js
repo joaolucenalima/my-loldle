d8030dad30220ad82acb5ad422f6ce2159c58321
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2024-03-02 20:01:30 -0300

    refactor: use appendChild instead innerHTML

--- script.js
+++ script.js
@@ -13,29 +13,33 @@ closeModalButton.addEventListener('click', () => {
 const input = document.querySelector(".guess-input")
 const list = document.querySelector(".champion-list")
 
-fetch('https://ddragon.leagueoflegends.com/cdn/14.4.1/data/pt_BR/champion.json')
+const champions = await fetch('https://ddragon.leagueoflegends.com/cdn/14.4.1/data/pt_BR/champion.json')
   .then(response => response.json())
-  .then(object => {
-    let champions = Object.keys(object.data)
+  .then(objetctResponse => Object.keys(objetctResponse.data))
+  .then(champList => {
+    champList.forEach((champion) => {
+      const imgUrl = `https://ddragon.leagueoflegends.com/cdn/14.4.1/img/champion/${champion}.png`
 
-    input.addEventListener('input', (event) => {
-      list.innerHTML = ""
+      const liElement = document.createElement("li")
+      liElement.className = "option"
+      liElement.innerHTML = `
+        <input type="radio" name="${champion}" id="${champion}" disabled>
+        <img src="${imgUrl}" width="48px" height="48px"/>
+        <p>${champion}</p>`
 
-      if (event.target.value == "") return
-
-      const filteredChampions = champions.filter(champion => champion.toLowerCase().includes(event.target.value))
-
-      filteredChampions.forEach((champion) => {
-
-        const imgUrl = `https://ddragon.leagueoflegends.com/cdn/14.4.1/img/champion/${champion}.png`
-
-        list.innerHTML += `
-          <li class="option">
-            <input type="radio" name="teste" id="teste">
-            <img src="${imgUrl}" width="48px" height="48px"/>
-            <p>${champion}</p>
-          </li>
-        `
-      })
+      list.appendChild(liElement)
     })
+
+    return champList
   })
+
+console.log(champions)
+
+input.addEventListener('input', (event) => {
+  if (event.target.value == "") return
+
+  champions.forEach((champion) => {
+    const matches = champion.toLowerCase().includes(event.target.value.toLowerCase())
+    document.querySelector(`#${champion}`).disabled = !matches
+  })
+})
